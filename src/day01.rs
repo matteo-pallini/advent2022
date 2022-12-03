2467dd95bfa41c9ea6cfcf51d78344afbde845fb
drabbit17 <matteo.pallini17@gmail.com>
2022-12-03 15:52:16 +0000

    Day 1

---
+++ src/day01.rs
@@ -0,0 +1,30 @@
+use min_max_heap::MinMaxHeap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn run() -> Option<u8> {
+    let mut current_value: u32 = 0;
+    let mut heap = MinMaxHeap::with_capacity(3);
+
+    let input = File::open("src/day01.txt").expect("file not found");
+    let buffered = BufReader::new(input);
+    for line in buffered.lines() {
+        if line.as_ref().unwrap().is_empty() {
+            if heap.len() <= 3 {
+                heap.push(current_value)
+            } else {
+                let unused: u32 = heap.push_pop_min(current_value);
+            }
+            current_value = 0
+        } else {
+            current_value = current_value + line.expect("no line").parse::<u32>().unwrap()
+        }
+    }
+
+    let mut max_value: u32 = heap.pop_max().unwrap_or(0);
+    println!("day1, max value is {}", max_value);
+    max_value = max_value + heap.pop_max().unwrap_or(0);
+    max_value = max_value + heap.pop_max().unwrap_or(0);
+    println!("day1, max 3 values are {}", max_value);
+    None
+}
