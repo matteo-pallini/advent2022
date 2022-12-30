e9b2c20c52a256e6130372313a8d205fa837b599
matteo-pallini <matteo.pallini@vortexa.com>
2022-12-30 20:45:56 +0000

    problem 6 - step 2

--- src/day06.rs
+++ src/day06.rs
@@ -8,11 +8,12 @@ pub fn run() -> Option<u8> {
         .unwrap()
         .chars()
         .collect();
+    let max_length: u8 = 14;
     let mut first_pointer: usize = 0;
     let mut second_pointer: usize = 0;
-    let mut counter: HashMap<char, u8> = HashMap::with_capacity(4);
-    while (second_pointer < message_chars.len()) && (counter.len() < 4) {
-        if second_pointer - first_pointer > 3 {
+    let mut counter: HashMap<char, u8> = HashMap::with_capacity(max_length as usize);
+    while (second_pointer < message_chars.len()) && (counter.len() < max_length as usize) {
+        if second_pointer - first_pointer > (max_length - 1) as usize {
             if *counter.get(&message_chars[first_pointer]).unwrap() == 1 as u8 {
                 counter.remove(&message_chars[first_pointer]);
             } else {
